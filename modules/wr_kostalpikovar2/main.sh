f80f80fbff3af728fd0b82e33a7284f63e10bf8c
hhoefling <penny@gmx.eu>
2022-03-31 17:31:23 +0200

    Alte module

--- modules/wr_kostalpikovar2/main.sh
+++ modules/wr_kostalpikovar2/main.sh
@@ -15,15 +15,3 @@ if [ ${DMOD} == "MAIN" ]; then
 else
 	MYLOGFILE="${RAMDISKDIR}/wr_kostalpikovar2.log"
 fi
-
-openwbDebugLog ${DMOD} 2 "WR User: ${wr_piko2_user}"
-openwbDebugLog ${DMOD} 2 "WR Passwort: ${wr_piko2_pass}"
-openwbDebugLog ${DMOD} 2 "WR URL: ${wr_piko2_url}"
-
-python3 /var/www/html/openWB/modules/wr_kostalpikovar2/kostal_piko_var2.py 1 "${wr_piko2_url}" "${wr_piko2_user}" "${wr_piko2_pass}" >>$MYLOGFILE 2>&1
-ret=$?
-
-openwbDebugLog ${DMOD} 2 "RET: ${ret}"
-
-pvwatt=$(</var/www/html/openWB/ramdisk/pvwatt) 
-echo $pvwatt
