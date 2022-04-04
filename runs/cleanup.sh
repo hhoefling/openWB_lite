ff1a8b9041c1969dd4b53d7205d7429bc0861f3f
hhoefling <penny@gmx.eu>
2022-04-04 19:22:54 +0200

    cleanup

--- runs/cleanup.sh
+++ runs/cleanup.sh
@@ -6,7 +6,11 @@ timestamp=`date +"%Y-%m-%d %H:%M:%S"`
 if [ -f /var/www/html/openWB/ramdisk/debuguser ]; then
 	echo "$timestamp cleanup.sh: Skipping logfile cleanup as senddebug.sh is collecting data." >> /var/www/html/openWB/ramdisk/openWB.log
 else
-  echo "$timestamp cleanup.sh: checking logfiles." >> /var/www/html/openWB/ramdisk/openWB.log
+        # Dateien die auf /var/log verlinken werden nicht verkleinert (macht logrotate)
+        echo "$timestamp cleanup.sh: checking logfiles (without openWB.log)" >> /var/www/html/openWB/ramdisk/openWB.log
+        echo "$timestamp cleanup.sh: checking logfiles (without openWB.log)"
   find /var/www/html/openWB/ramdisk/ -name "*.log" -type f -exec /var/www/html/openWB/runs/cleanupf.sh {} \;
-  /var/www/html/openWB/runs/cleanupf.sh /var/log/openWB.log
+        echo "$timestamp cleanup.sh: checking openWB.log 512Kb" >> /var/www/html/openWB/ramdisk/openWB.log
+        echo "$timestamp cleanup.sh: checking openWB.log 512Kb"
+        /var/www/html/openWB/runs/cleanupf.sh /var/log/openWB.log  512;
 fi
--- runs/cleanupf.sh
+++ runs/cleanupf.sh
@@ -4,15 +4,16 @@
 # Truncate only if bigger, let the file untouched if less.
 
 f=$1
+kb=${2:-256}
 
 logfilesize=$(stat --format=%s "$f")
-if  (( $logfilesize > (256*1024) )) ; then
+if  (( $logfilesize > (kb * 1024) )) ; then
     timestamp=`date +"%Y-%m-%d %H:%M:%S"`
     lines=$(wc -l <"$f" )
     lines=$(( $lines / 4 ))   # truncate to 1/4 size
     echo "$(tail -$lines $f)" > $f
     echo "$timestamp cleanupf.sh truncate tp $lines" >>$f
-    echo "$timestamp cleanupf.sh Logfile $f bigger than 256Kb, truncate it to $lines lines"
+    echo "$timestamp cleanupf.sh Logfile $f bigger than $kb Kb, truncate it to $lines lines"
     chown pi:pi $f
     chmod a+rw $f
     #ls -l $f
