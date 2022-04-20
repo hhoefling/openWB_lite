0879f0ccceef2a2fcc9ed31c23763ce88288c538
hhoefling <penny@gmx.eu>
2022-04-20 20:36:28 +0200

    Update main.sh

--- modules/bezug_rct2/main.sh
+++ modules/bezug_rct2/main.sh
@@ -1,51 +1,20 @@
 #!/bin/bash
-OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
-stamp="$OPENWBBASEDIR/ramdisk/rct2.last"
+OPENWBBASEDIR=$(cd "$(dirname "$0")/../../" && pwd)
+RAMDISKDIR="${OPENWBBASEDIR}/ramdisk"
+#DMOD="EVU"
+DMOD="MAIN"
 
-# check if config file is already in env
-if [[ -z "$debug" ]]; then
-	. $OPENWBBASEDIR/loadconfig.sh
-	. $OPENWBBASEDIR/helperFunctions.sh
-fi
-debug=${1:-$debug}
-secs=${2:-"300"}
-
-
-function Log()
-{
- level=$1;
- shift;
- openwbDebugLog "MAIN" $level "rct2: $*"
-}
-
-
-now=`date +%s`
-# check Lastrun
-if [ -f "$stamp" ] ; then
-	lastrun=$(<$stamp)
-else	
-	lastrun=0
-	echo "0" >$stamp
-	chmod a+rw $stamp
-fi
-diff="$((now-lastrun))"
-if (( diff >= secs )) ; then  # alle 5 Minuten 
-   echo $now  >$stamp
-   m5="--m5"
-   Log  1 "##### fire --m5 event ##### ($debug $secs)"
-   (( debug > 2 )) && echo "##### fire --m5 event ##### ($debug $secs)"
+if [ ${DMOD} == "MAIN" ]; then
+	MYLOGFILE="${RAMDISKDIR}/openWB.log"
 else
-   m5=""
-   Log 2 "Last --m5  $diff sec ago, skip for now ($debug)"
-   (( debug > 2 )) && echo "Last --m5  $diff sec ago, skip for now ($debug $secs)"
+	MYLOGFILE="${RAMDISKDIR}/evu.log"
 fi
 
-if (( debug > 2 )) ; then
-  python3 /var/www/html/openWB/modules/bezug_rct2/rct2.py --verbose --ip=$bezug1_ip  -b=$wattbezugmodul -w=$pvwattmodul -s=$speichermodul $m5 >>/var/log/openWB.log 2>&1 
-else
-  python3 /var/www/html/openWB/modules/bezug_rct2/rct2.py --ip=$bezug1_ip  -b=$wattbezugmodul -w=$pvwattmodul -s=$speichermodul $m5  >>/var/log/openWB.log 2>&1
-fi 
+# Exportere Volt/Ampere/Frequenz etc in die Ramdisk
+bash "$OPENWBBASEDIR/packages/legacy_run.sh" "bezug_rct2.rct_read_bezug" "${bezug1_ip}" >>"${MYLOGFILE}" 2>&1
+ret=$?
+
+openwbDebugLog ${DMOD} 2 "RET: ${ret}"
 
 # Nehme wattbezug als ergbenis mit zurueck da beim Bezug-Module ein Returnwert erwartet wird.
-cat  /var/www/html/openWB/ramdisk/wattbezug
-
+cat "${RAMDISKDIR}/wattbezug"
