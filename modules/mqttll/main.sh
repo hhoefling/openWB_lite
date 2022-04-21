8133b6213838251d50c3c80040e4c519f292dc51
hhoefling <penny@gmx.eu>
2022-04-21 18:38:08 +0200

    Update main.sh

--- modules/mqttll/main.sh
+++ modules/mqttll/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # must be called  as pi from /var/www/html/openWB
-OPENWBBASEDIR=$(cd `dirname $0`/../ && pwd)
+OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
 RAMDISKDIR="$OPENWBBASEDIR/ramdisk"
 MODULEDIR=$(cd `dirname $0` && pwd)
 
