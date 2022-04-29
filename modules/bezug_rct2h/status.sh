b14879861ad909ce74a1b252cc04b5ae10e20cb6
hhoefling <penny@gmx.eu>
2022-04-29 11:06:18 +0200

    Update status.sh

--- modules/bezug_rct2h/status.sh
+++ modules/bezug_rct2h/status.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 SELF=$(cd `dirname $0`   &&  pwd)
+OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
+MODULEDIR=$(cd `dirname $0` && pwd)
+
+# check if config file is already in env
+if [[ -z "$debug" ]]; then
+	. $OPENWBBASEDIR/loadconfig.sh
+fi
 
 if [ -n "$bezug1_ip" ]; then
-  opt=""
+  opt=" -v --info"
 else
   echo "$0 Debughilfe bezug1_ip parameter not supplied use 192.168.208.63"
   bezug1_ip=192.168.208.63
