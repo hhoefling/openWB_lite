e68c047434cbd474bc4a59d7a17d4d889ba696af
hhoefling <penny@gmx.eu>
2023-03-02 18:36:03 +0100

    rct

--- modules/wr_rct2h/main.sh
+++ modules/wr_rct2h/main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# aus modules/speicher_rctxx/main.sh  modules/bezug_rctxx/speicher_main.sh machen
+# aus modules/wr_rctxx/main.sh  modules/bezug_rctxx/wr_main.sh machen
 B=${0/wr_/bezug_}
 B=${B/main.sh/wr_main.sh}
 $B
