ded1cfce383bddafa454185159bdf9b8869c2f33
hhoefling <penny@gmx.eu>
2023-04-04 12:44:38 +0200

    Update cleanup.sh

--- runs/cleanup.sh
+++ runs/cleanup.sh
@@ -4,11 +4,13 @@
 #
 timestamp=`date +"%Y-%m-%d %H:%M:%S"`
 #if [ -f /var/www/html/openWB/ramdisk/debuguser ]; then
-#	echo "$timestamp cleanup.sh: Skipping logfile cleanup as senddebug.sh is collecting data." >> /var/www/html/openWB/ramdisk/openWB.log
+#   echo "$timestamp cleanup.sh: Skipping logfile cleanup as senddebug.sh is collecting data." >> /var/www/html/openWB/ramdisk/openWB.log
 #else
-	# Dateien die auf /var/log verlinken werden nicht verkleinert (macht logrotate)
-	echo "$timestamp cleanup.sh: checking logfiles" >> /var/www/html/openWB/ramdisk/openWB.log
-	echo "$timestamp cleanup.sh: checking logfiles"
-	find /var/www/html/openWB/ramdisk/ -name "*.log" -type f -exec /var/www/html/openWB/runs/cleanupf.sh {} \;
-	/var/www/html/openWB/runs/cleanupf.sh /var/log/openWB.log  4096;
+    # Dateien die auf /var/log verlinken werden nicht verkleinert 
+    echo "$timestamp cleanup.sh: checking logfiles" >> /var/www/html/openWB/ramdisk/openWB.log
+    echo "$timestamp cleanup.sh: checking logfiles"
+    find /var/www/html/openWB/ramdisk/ -name "*.log" -type f -exec /var/www/html/openWB/runs/cleanupf.sh {} \;
+    # nun die verlinkte ebenfalls 
+    /var/www/html/openWB/runs/cleanupf.sh /var/log/openWB.log  4096;
 #fi
+
