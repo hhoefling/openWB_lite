aea677a03ccdf29cac036323417bca747c7ca01f
hhoefling <penny@gmx.eu>
2023-04-04 12:33:24 +0200

    Update cleanupf.sh

--- runs/cleanupf.sh
+++ runs/cleanupf.sh
@@ -5,7 +5,7 @@
 # runs from cleanup.sh as root
 
 f=$1
-kb=${2:-1024}
+kb=${2:-2048}
 
 logfilesize=$(stat --format=%s "$f")
 #echo  $logfilesize $kb 
@@ -15,7 +15,7 @@ if  (( $logfilesize > (kb * 1024) )) ; then
     lines=$(( $linesges / 4 ))   # truncate to 1/4 size
     echo "$(tail -$lines $f)" > $f
     echo "$timestamp cleanupf.sh truncate $linesges to $lines" >>$f
-    echo "$timestamp cleanupf.sh Logfile $f bigger than $kb Kb, truncate $linesges to $lines lines"
+    echo "$timestamp cleanupf.sh Logfile $f $logfilesize > $kb Kb, truncate $linesges to $lines lines"
     chown pi:pi $f
     chmod a+rw $f
     #ls -l $f
