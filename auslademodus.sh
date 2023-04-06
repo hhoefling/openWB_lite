4955d019c92f23efc9d435bad8490a3ac1d973dd
hhoefling <penny@gmx.eu>
2023-04-06 22:25:50 +0200

    Update auslademodus.sh

--- auslademodus.sh
+++ auslademodus.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-auslademodus(){
 
+# Import ladeleistung
+# Export None, set new Current to Hardware
+
+ 
+# If Modue=STOP 
+auslademodus(){
+	local ladeleistung=${ladeleistung:-0}
+	openwbDebugLog "MAIN" 2 "auslademodus: Ladeleistung $ladeleistung"
 	if grep -q 1 ramdisk/ladestatus; then
-		meld "stop m"
+		meld "force stop m"
 		runs/set-current.sh 0 m
 		openwbDebugLog "CHARGESTAT" 0 "LP1, Lademodus Stop. Stoppe Ladung"
 	fi
 	if grep -q 1 "ramdisk/ladestatuss1"; then
-		meld "stop s1"
+		meld "force stop s1"
 		runs/set-current.sh 0 s1
 		openwbDebugLog "CHARGESTAT" 0 "LP2, Lademodus Stop. Stoppe Ladung"
 	fi
 	if grep -q 1 ramdisk/ladestatuss2; then
-		meld "stop s2"
+		meld "force stop s2"
 		runs/set-current.sh 0 s2
 		openwbDebugLog "CHARGESTAT" 0 "LP3, Lademodus Stop. Stoppe Ladung"
 	fi
 	if (( ladeleistung > 300 )); then
+		meld "force stop m,s1,s2"
 		runs/set-current.sh 0 m
 		runs/set-current.sh 0 s1
 		runs/set-current.sh 0 s2
@@ -27,6 +35,10 @@ auslademodus(){
 	openwbDebugLog "MAIN" 0 "*** exit 0 (auslademodus)"
 	exit 0
 }
+
+# If Mode=STANDBY and no Nacht/Morgen
+# Inport - None
+# export - None
 
 semiauslademodus(){
 	if grep -q 1 ramdisk/ladestatus; then
