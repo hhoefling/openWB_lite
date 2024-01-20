fb57935e3c05177a0cfe2e102c4d265ff1fcbd7f
hhoefling <penny@gmx.eu>
2024-01-20 20:20:03 +0100

    Update info.sh

--- modules/wr_rct2/info.sh
+++ modules/wr_rct2/info.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
-OPENWBBASEDIR=$(cd `dirname $0`/../../ && pwd)
-RAMDISKDIR="$OPENWBBASEDIR/ramdisk"
-MODULEDIR=$(cd `dirname $0` && pwd)
-CONFIGFILE="$OPENWBBASEDIR/openwb.conf"
-
-# check if config file is already in env
-if [[ -z "$debug" ]]; then
-	. $OPENWBBASEDIR/loadconfig.sh
-fi
-
-
-# Call readmodule from bezug_rct2    
-timeout -k 9 3 python3 /var/www/html/openWB/modules/bezug_rct2/rct_read_pv_info.py --ip=$bezug1_ip  
-rc=$?
-if  [[ ($rc == 143)  || ($rc == 124) ]] ; then
-  echo "PV-Info Script timed out"
-fi
-exit $rc
-
-
+# aus modules/wr_rctxx/info.sh  modules/bezug_rctxx/wr_info.sh machen
+B=${0/wr_/bezug_}
+B=${B/info.sh/wr_info.sh}
+. $B
