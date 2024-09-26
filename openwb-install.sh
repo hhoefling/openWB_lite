538212d54ac83bbb2e06705747d19590a9dfe2ae
Penny <89247538+hhoefling@users.noreply.github.com>
2024-09-26 17:56:00 +0200

    Update openwb-install.sh
    change paho-mqtt to 1.x

--- openwb-install.sh
+++ openwb-install.sh
@@ -239,7 +239,8 @@ echo "check for paho-mqtt"
 if python3 -c "import paho.mqtt.publish as publish" &> /dev/null; then
 	echo 'mqtt installed...'
 else
-	sudo pip3 install paho-mqtt
+	# sudo pip3 install paho-mqtt
+        sudo pip3 install "paho-mqtt<2.0.0"
 fi
 
 
